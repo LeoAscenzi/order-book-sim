6cff17576ebfd6bf4f21feb8a97a3e616d9173f3
Leo Ascenzi <leo.ascenzi@gmail.com>
2025-12-08 20:22:00 -0500

    oh linter matters

--- dashboard/src/DataTables.tsx
+++ dashboard/src/DataTables.tsx
@@ -3,13 +3,13 @@ import { useEffect } from "react";
 function DataTables()
 {
 
-    type OrderBook  = {
-    [security: string] :
-    {
-        bids: Record<string, string>;
-        asks: Record<string, string>;
-    };
-    }
+    // type OrderBook  = {
+    // [security: string] :
+    // {
+    //     bids: Record<string, string>;
+    //     asks: Record<string, string>;
+    // };
+    // }
 
     useEffect(() => {
         console.log("fetching all data");   
